07902a4a393a87dc9bc72215e0b45df0047b09e5
Christian Fischer <sw-dev@computerlyrik.de>
2015-11-14 20:15:17 +0100

    initial

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             "application_mvn"
+maintainer       "computerlyrik"
+maintainer_email "cookbooks@computerlyrik.de"
+license          "Apache 2.0"
+description      "Run Maven-configured Applications"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "application"
+depends "maven"
